aac643efb95903e7f5cfe3a6ca7d476651180295
cloudyan <1395093509@qq.com>
2021-09-24 18:29:30 +0800

    fix: package

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 // console.log(require.resolve(path.join(__dirname, './', 'dist/eslint')));
 module.exports = {
+  root: true, // js 项目检测需要配置此项
   extends: [require.resolve(path.join(__dirname, './', 'dist/eslint'))],
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
