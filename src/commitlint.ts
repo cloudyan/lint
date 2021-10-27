4ec143f7020824a21495152bd02d24f530c64486
cloudyan <1395093509@qq.com>
2021-10-27 14:04:52 +0800

    feat: use pnpm

--- src/commitlint.ts
+++ src/commitlint.ts
@@ -1,11 +1,11 @@
 export default {
-  extends: [
-    '@commitlint/config-conventional',
-  ],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', [
-      'upd', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert',
-    ]],
+    'type-enum': [
+      2,
+      'always',
+      ['upd', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert'],
+    ],
     'type-case': [0],
     'type-empty': [0],
     'scope-empty': [0],
