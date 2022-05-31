799ecb2708d989ccf6223359b09260c46fa926f1
cloudyan <1395093509@qq.com>
2022-05-31 17:05:27 +0800

    fix: deps

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+import eslint = require('./config/eslint')
+import prettier = require('./config/prettier')
+import stylelint = require('./config/stylelint')
+import commitlint = require('./config/commitlint')
+import browserslist = require('./config/browserslist')
+
+export { eslint, prettier, stylelint, commitlint, browserslist, browserslist as default }
